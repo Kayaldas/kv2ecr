4105c1080aacea21e638c37f95634f3aca3b34af
kayal <kayal241987@gmail.com>
2025-04-28 09:55:29 -0400

    code

---
+++ src/main/java/com/example/Springboot_ecrdemo/SpringbootEcrdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_ecrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcrdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcrdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springboot_ecrdemo/SpringbootEcrdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_ecrdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcrdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
